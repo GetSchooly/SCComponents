3c48b2fa8afcf6a0b253c47c6b8bba87e2b2a1a3
Ankit Kumar <akki6230@hotmail.com>
2024-10-05 08:26:05 +0530

    updated code

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/akki6230/SCTokens", .upToNextMajor(from: Version("1.0.0")))
+        .package(url: "https://github.com/GetSchooly/SCTokens", .upToNextMajor(from: Version("1.0.0")))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
