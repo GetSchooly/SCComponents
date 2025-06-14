c582d406a46a2bf8a61ee6333cd33a64585e834c
Ankit Kumar <akki6230@hotmail.com>
2025-06-14 19:08:55 +0530

    added UserDefault

---
+++ Sources/SDComponents/Sources/Core/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+public final class UserDefaultsManager {
+    
+    public static let shared = UserDefaultsManager()
+    
+    private let defaults = UserDefaults.standard
+
+    private init() {}
+
+    // MARK: - Keys
+    private enum Keys {
+        static let isLoggedIn = "isLoggedIn"
+        static let gurardianEmailId = "gurardianEmailId"
+        static let lastLoginDate = "lastLoginDate"
+    }
+
+    // MARK: - Stored Properties
+    public var isLoggedIn: Bool {
+        get { defaults.bool(forKey: Keys.isLoggedIn) }
+        set { defaults.set(newValue, forKey: Keys.isLoggedIn) }
+    }
+
+    public var gurardianEmailId: String? {
+        get { defaults.string(forKey: Keys.gurardianEmailId) }
+        set { defaults.set(newValue, forKey: Keys.gurardianEmailId) }
+    }
+
+    public var lastLoginDate: Date? {
+        get { defaults.object(forKey: Keys.lastLoginDate) as? Date }
+        set { defaults.set(newValue, forKey: Keys.lastLoginDate) }
+    }
+
+    // MARK: - Clear User or All
+    public func clearUserData() {
+        defaults.removeObject(forKey: Keys.gurardianEmailId)
+        defaults.removeObject(forKey: Keys.isLoggedIn)
+        defaults.removeObject(forKey: Keys.lastLoginDate)
+    }
+    
+    public func clearAll() {
+        for key in defaults.dictionaryRepresentation().keys {
+            defaults.removeObject(forKey: key)
+        }
+    }
+}
